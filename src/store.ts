bcf927b83574d228afb2614e942fdaf3a83c1728
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-20 06:21:58 -0700

    ReduxToolkit

--- src/store.ts
+++ src/store.ts
@@ -2,6 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import FrameObject from "./Slices/FrameObject";
 const Store = configureStore({
     reducer: {
+        /** FrameObject is Reducer which we use it at Selectors using useSelector Hook */
+        /** const Value = useSelector((state: any) => state.FrameObject.value); */
+        /** initialState is value at const Value = useSelector((state: any) => state.FrameObject.value); */
+        /** -------------------------------------------------------------------------------------------- */
+        /** const FrameObject = createSlice({
+            name: "FrameObject",
+            initialState: { value: 0 },
+            reducers: {
+                Increment, Decrement
+             }
+        }) */
+        /** -------------------------------------------------------------------------------------------- */
         FrameObject: FrameObject
     }
 })
