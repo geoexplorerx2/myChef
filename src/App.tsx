6c817aa7ce6babee9fa47c4e4171b3f968b4f49a
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-20 05:02:46 -0700

    prepare

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import useWindowSize from './Hooks/useWindowSize';
 function App() {
+  /** Destructure the values returned by the 'useWindowSize()' hook into 'width' and 'height' variables. */
+  const { width, height } = useWindowSize()
   return (
     <main className="w-full min-h-screen bg-slate-800">
-     
+      <section className='w-full h-full text-white'>{width}</section>
     </main>
   );
 }
