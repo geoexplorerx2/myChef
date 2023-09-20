bc57316c13c723cbe627bd0a642fe45f7c071481
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-20 05:34:57 -0700

    PrepareRedux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import Store from './store';
+import { Provider } from 'react-redux';
 import useWindowSize from './Hooks/useWindowSize';
 import { MagnifierComponentFram } from './Components/MagnifierComponent/Index';
 function App() {
   /** Destructure the values returned by the 'useWindowSize()' hook into 'width' and 'height' variables. */
   const { width, height } = useWindowSize()
   return (
-    <main className="w-full min-h-screen bg-slate-800">
-      <MagnifierComponentFram/>
-    </main>
+    <Provider store={Store}>
+      <main className="w-full min-h-screen bg-slate-800">
+        <MagnifierComponentFram />
+      </main>
+    </Provider>
   );
 }
 
