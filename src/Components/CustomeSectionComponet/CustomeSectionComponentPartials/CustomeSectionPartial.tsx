8caa2185c75bd13c00953667284b0f9a50957c2f
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-21 05:47:47 -0700

    NameOfProgressSection

--- src/Components/CustomeSectionComponet/CustomeSectionComponentPartials/CustomeSectionPartial.tsx
+++ src/Components/CustomeSectionComponet/CustomeSectionComponentPartials/CustomeSectionPartial.tsx
@@ -28,6 +28,9 @@ const CustomeSectionPartial: FC<CustomeSectionPartial> = ({ windowSize }) => {
                                 <div className='w-[35px] h-[35px] bg-[#4C6486] rounded-full flex justify-center items-center'>
                                     <span className='text-sm text-white'>1</span>
                                 </div>
+                                <div className='absolute top-0 left-0 w-full h-full flex items-end translate-y-8'>
+                                    <span className='text-black text-sm'>İsim</span>
+                                </div>
                             </div>
                             {/* ---------------------------------------------------------------------------- */}
                             <div className='w-[15%] flex items-center'>
@@ -37,6 +40,9 @@ const CustomeSectionPartial: FC<CustomeSectionPartial> = ({ windowSize }) => {
                             <div className='w-[10%] relative'>
                                 <div className='w-[35px] h-[35px] bg-[#fff] rounded-full flex justify-center items-center border-[1px] border-[rgba(0,0,0,0.3)]'>
                                     <span className='text-sm text-black'>2</span>
+                                </div>
+                                <div className='absolute top-0 left-0 w-full h-full flex items-end translate-y-8 -translate-x-5'>
+                                    <span className='text-black text-sm'>Logo/Marka</span>
                                 </div>
                             </div>
                             {/* ---------------------------------------------------------------------------- */}
@@ -48,6 +54,9 @@ const CustomeSectionPartial: FC<CustomeSectionPartial> = ({ windowSize }) => {
                                 <div className='w-[35px] h-[35px] bg-[#fff] rounded-full flex justify-center items-center border-[1px] border-[rgba(0,0,0,0.3)]'>
                                     <span className='text-sm text-black'>3</span>
                                 </div>
+                                <div className='absolute top-0 left-0 w-full h-full flex items-end translate-y-8'>
+                                    <span className='text-black text-sm'>Bayrak</span>
+                                </div>
                             </div>
                             {/* ---------------------------------------------------------------------------- */}
                             <div className='w-[15%] flex items-center'>
@@ -57,6 +66,9 @@ const CustomeSectionPartial: FC<CustomeSectionPartial> = ({ windowSize }) => {
                             <div className='w-[10%] relative'>
                                 <div className='w-[35px] h-[35px] bg-[#fff] rounded-full flex justify-center items-center border-[1px] border-[rgba(0,0,0,0.3)]'>
                                     <span className='text-sm text-black'>4</span>
+                                </div>
+                                <div className='absolute top-0 left-0 w-full h-full flex items-end translate-y-8 translate-x-1'>
+                                    <span className='text-black text-sm'>Özet</span>
                                 </div>
                             </div>
                             {/* ---------------------------------------------------------------------------- */}
