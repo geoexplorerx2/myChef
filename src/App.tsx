50964cebf12adb088d638315f931a132ecad6443
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-22 00:08:15 -0700

    designProblem

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,26 @@ function App() {
   const { width, height } = useWindowSize()
   return (
     <Provider store={Store}>
-      <main className="w-full min-h-screen relative">
-        <NavBar />
-        <CustomeSection />
-        <div className='w-full flex justify-center'>
-          <div className='w-[90%] border-2 mt-20 rounded-2xl p-5'>
-            <MagnifierComp
-              width={"100%"}
-              src="https://chefworks.liquifire.com/chefworks?set=previewSize[large],scaleFactor[3],prodID[JLCV],prodColor[BLU],prodView[front],textPosition[],flag1[],flag1Position[],flag2[],flag2Position[],seed[001]&call=url[file:main]&sink=format[jpg]"
-            />
+      {/* <Frame/> */}
+      <main className="w-full min-h-screen relative bg-white">
+        <section className='sticky top-0 w-full bg-white'>
+          <NavBar />
+          <div className='w-full relative'>
+            <div className='sticky top-0'>
+              <CustomeSection />
+              <div className='w-full flex justify-center'>
+                <div className='w-[90%] border-2 mt-20 rounded-2xl p-5'>
+                  <MagnifierComp
+                    width={"100%"}
+                    src="https://chefworks.liquifire.com/chefworks?set=previewSize[large],scaleFactor[3],prodID[JLCV],prodColor[BLU],prodView[front],textPosition[],flag1[],flag1Position[],flag2[],flag2Position[],seed[001]&call=url[file:main]&sink=format[jpg]"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
+        </section>
+        <div>
+          Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
         </div>
       </main>
     </Provider>
