9a98c725a2d1ae88347b2816c59d6df937917ef9
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-22 00:51:34 -0700

    Designproblem

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import NavBar from './Components/NavBarComp/Index';
 import CustomeSection from './Components/CustomeSectionComp/Index';
 import MagnifierComp from './Components/MagnifierComp/Index';
 import ProgressComp from './Components/ProgressSectionComp/Index';
+import NameComp from './Components/NameSectionComp/Index';
 function App() {
   /** Destructure the values returned by the 'useWindowSize()' hook into 'width' and 'height' variables. */
   const { width, height } = useWindowSize()
@@ -32,11 +33,11 @@ function App() {
             </div>
           </div>
         </section>
-        <div>
-          Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
-          Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
-          Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
-          Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
+        <div className='w-full'>
+          <NameComp />
+        </div>
+        <div className='w-full'>
+
         </div>
       </main>
     </Provider>
---
+++ src/Components/NameSectionComp/Index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import add from '../../assets/svg/add.svg'
+const Index = () => {
+    return (
+        <div className='w-full'>
+            <div className='px-7 pt-6 text-base font-semibold'>Isim</div>
+            <div className='w-full px-7 py-3'>
+                <input type='text' className='w-full h-12 rounded-md px-5 bg-[#E8E9EB] border-[1px] border-[#E8E9EB] text-[rgba(0,0,0,0.7)]' placeholder='İsim Yaz' />
+            </div>
+            <div className='w-full px-7 py-3'>
+                <button className='w-full  flex justify-center items-center h-12 rounded-md px-5 border-[1px] border-[#E8E9EB] text-[rgba(0,0,0,0.7)]'>
+                    <div className='flex'>
+                        <span className='mx-2'><img src={add} /></span>
+                        <span>Ek İsim Ekle</span>
+                    </div>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Index
