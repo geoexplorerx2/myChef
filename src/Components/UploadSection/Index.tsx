29f3a7b341b11ec2b899a5311f26d87d420b9d98
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-22 06:40:03 -0700

    Page2

---
+++ src/Components/UploadSection/Index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import add from '../../assets/svg/add.svg'
+const Index = () => {
+  return (
+    <div className='w-full h-[140px] flex justify-center mb-10'>
+      <div className='w-[90%] h-full border-[1px] border-dashed border-[rgba(0,0,0,0.8)] rounded-md'>
+         <div className='w-full flex justify-center pt-5'>
+            <img className='w-[8%]' src={add}/>
+         </div>
+         <div className='w-full flex justify-center pt-3'>
+            <span className='text-sm font-bold'>Logo Yükle</span>
+         </div>
+         <div className='w-full flex justify-center pt-3'>
+            <span className='text-xs text-[rgba(0,0,0,0.5)]'>Şeffaf arka plana sahip PNG görüntüleri olarak yükleyin</span>
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default Index
--- src/Pages/Page2.tsx
+++ src/Pages/Page2.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Logo from '../Components/LogoUploadComp/Index'
+import UploadComp from '../Components/UploadSection/Index'
 const Page2 = () => {
     return (
         <div className='w-full'>
             <div className='w-full'>
                 <Logo />
+            </div>
+            <div className='w-full'>
+                <UploadComp/>
             </div>
         </div>
     )
