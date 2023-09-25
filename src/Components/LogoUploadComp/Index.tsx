091d7f8eb542ec2a6e15c7920273d7c3751f890c
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-25 01:47:10 -0700

    LogoMarker

--- src/Components/LogoUploadComp/Index.tsx
+++ src/Components/LogoUploadComp/Index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import LeftSignSymbol from '../../assets/svg/leftSignSymbol.svg'
 const Index = () => {
   return (
     <div className='w-full px-7 bg-white logo-section-shadow'>
       <div className='w-full flex items-center pt-8'>
-        <div className='w-[20%] text-xl font-bold text-[rgba(0,0,0,0.6)]'>{'<'}</div>
+        <div className='w-[20%]'><img src={LeftSignSymbol}/></div>
         <div className='text-2xl font-semibold'>Logo / Marka Yükle</div>
       </div>
       <div className='w-full flex justify-center py-3 mb-5'>
