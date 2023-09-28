93dc9531c2574d5012a17837d0b457f4b49f75d5
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-28 04:42:34 -0700

    add

--- src/Components/ColorSectionComp/Index.tsx
+++ src/Components/ColorSectionComp/Index.tsx
@@ -1,6 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import whiteTick from '../../assets/svg/whiteCheck.svg'
+import { useDispatch } from 'react-redux'
 const Index = () => {
+    const dispatch = useDispatch()
+    const [elem, setElem] = useState('#333333')
+    const elements = [
+        {
+            color: 'bg-[#1694BA]',
+            data: '#1694BA',
+            el: <img className='w-[40%]' src={whiteTick} />
+        },
+        {
+            color: 'bg-[#F08A12]',
+            data: '#F08A12',
+            el: <img className='w-[40%]' src={whiteTick} />
+        },
+        {
+            color: 'bg-[#7FA538]',
+            data: '#7FA538',
+            el: <img className='w-[40%]' src={whiteTick} />
+        },
+        {
+            color: 'bg-[#EF54D1]',
+            data: '#EF54D1',
+            el: <img className='w-[40%]' src={whiteTick} />
+        },
+        {
+            color: 'bg-[#333333]',
+            data: '#333333',
+            el: <img className='w-[40%]' src={whiteTick} />
+        },
+    ]
+    const handleColor = (color: any) => {
+        setElem(color)
+    }
     return (
         <div className='w-full'>
             <div className='px-7 text-base font-semibold pb-1'>Renk</div>
@@ -10,24 +43,17 @@ const Index = () => {
                         <div className='px-5 py-1'><span className='font-semibold text-base'>Seçili Renk</span> :<span className='mx-1 text-[#F08A12] font-semibold text-base'>Orman Turuncusu</span></div>
                     </div>
                     <div className='w-full flex items-center justify-between px-5 py-6'>
-                        <div className='w-[35px] h-[35px] rounded-full bg-[#1694BA]'>
-
-                        </div>
-                        <div className='w-[35px] h-[35px] rounded-full bg-[#F08A12]'>
-
-                        </div>
-                        <div className='w-[35px] h-[35px] rounded-full bg-[#7FA538]'>
-
-                        </div>
-                        <div className='w-[35px] h-[35px] rounded-full bg-[#EF54D1]'>
-
-                        </div>
-                        <div className='w-[35px] h-[35px] rounded-full bg-[#333333] flex justify-center items-center'>
-                          <img className='w-[40%]' src={whiteTick}/>
-                        </div>
-                        <div className='w-[35px] h-[35px] rounded-full bg-[#ffffff] border-[1px] border-[#E3E3E3]'>
-
-                        </div>
+                        {
+                            elements.map((item: any) => {
+                                return (
+                                    <>
+                                        <div onClick={() => handleColor(item.data)} className={`w-[35px] h-[35px] rounded-full ${item.color} flex justify-center items-center`}>
+                                            {(elem == item.data ? item.el : null)}
+                                        </div>
+                                    </>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
