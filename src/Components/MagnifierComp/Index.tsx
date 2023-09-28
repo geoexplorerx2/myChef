885f852751bc99f92ccb4cc07d3610951b8f7fe9
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-28 03:21:41 -0700

    addingNameOnWearing

--- src/Components/MagnifierComp/Index.tsx
+++ src/Components/MagnifierComp/Index.tsx
@@ -1,96 +1,91 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback, useMemo } from 'react';
 import { useSelector } from 'react-redux';
+
 const Index = ({
-    src,
-    width,
-    height,
-    magnifierHeight = 200,
-    magnifieWidth = 200,
-    zoomLevel = 2
+  src,
+  width,
+  height,
+  magnifierHeight = 200,
+  magnifieWidth = 200,
+  zoomLevel = 2,
 }: {
-    src: string;
-    width?: string;
-    height?: string;
-    magnifierHeight?: number;
-    magnifieWidth?: number;
-    zoomLevel?: number;
+  src: string;
+  width?: string;
+  height?: string;
+  magnifierHeight?: number;
+  magnifieWidth?: number;
+  zoomLevel?: number;
 }) => {
-    const [[x, y], setXY] = useState([0, 0]);
-    const [[imgWidth, imgHeight], setSize] = useState([0, 0]);
-    const [showMagnifier, setShowMagnifier] = useState(false);
-    let Name = (useSelector((state: any) => state.NameSlice.name))
-    console.log(Name.payload)
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
+  const [showMagnifier, setShowMagnifier] = useState(false);
 
-    return (
+  const handleMouseEnter = useCallback((e:any) => {
+    const elem = e.currentTarget;
+    const { width, height } = elem.getBoundingClientRect();
+    setImageSize({ width, height });
+    setShowMagnifier(true);
+  }, []);
+
+  const handleMouseMove = useCallback((e:any) => {
+    const elem = e.currentTarget;
+    const { top, left } = elem.getBoundingClientRect();
+    const x = e.pageX - left - window.pageXOffset;
+    const y = e.pageY - top - window.pageYOffset;
+    setPosition({ x, y });
+  }, []);
+
+  const handleMouseLeave = useCallback(() => {
+    setShowMagnifier(false);
+  }, []);
+
+  const name = useSelector((state: any) => state.NameSlice.name);
+  const formattedName = useMemo(() => name.payload, [name]);
+
+  return (
+    <div
+      style={{
+        position: 'relative',
+        height,
+        width,
+      }}
+    >
+      <div
+        className="relative"
+        style={{ height, width }}
+        onMouseEnter={handleMouseEnter}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+      >
+        <img src={src} alt="img" />
         <div
-            style={{
-                position: "relative",
-                height: height,
-                width: width
-            }}
+          className="absolute w-[70px] flex justify-start top-[120px] left-[160px] text-xs font-bold text-black z-50 overflow-hidden"
         >
-            <div
-                className='relative'
-                style={{ height: height, width: width }}
-                onMouseEnter={(e) => {
-                    // update image size and turn-on magnifier
-                    const elem = e.currentTarget;
-                    const { width, height } = elem.getBoundingClientRect();
-                    setSize([width, height]);
-                    setShowMagnifier(true);
-                }}
-                onMouseMove={(e) => {
-                    // update cursor position
-                    const elem = e.currentTarget;
-                    const { top, left } = elem.getBoundingClientRect();
-
-                    // calculate cursor position on the image
-                    const x = e.pageX - left - window.pageXOffset;
-                    const y = e.pageY - top - window.pageYOffset;
-                    setXY([x, y]);
-                }}
-                onMouseLeave={() => {
-                    // close magnifier
-                    setShowMagnifier(false);
-                }}
-            >
-                <img src={src} alt={"img"} />
-                <div className="absolute w-[70px] flex justify-start top-[120px] left-[160px] text-xs font-bold text-black z-50 overflow-hidden">
-                    <span className="w-full break-all ">{Name.payload}</span>
-                </div>
-            </div>
-
-
-            {/* <div
-                style={{
-                    display: showMagnifier ? "" : "none",
-                    position: "absolute",
-
-                    // prevent maginier blocks the mousemove event of img
-                    pointerEvents: "none",
-                    // set size of magnifier
-                    height: `${magnifierHeight}px`,
-                    width: `${magnifieWidth}px`,
-                    // move element center to cursor pos
-                    top: `${y - magnifierHeight / 2}px`,
-                    left: `${x - magnifieWidth / 2}px`,
-                    opacity: "1", // reduce opacity so you can verify position
-                    border: "1px solid lightgray",
-                    backgroundColor: "white",
-                    backgroundImage: `url('${src}')`,
-                    backgroundRepeat: "no-repeat",
-
-                    //calculate zoomed image size
-                    backgroundSize: `${imgWidth * zoomLevel}px ${imgHeight * zoomLevel
-                        }px`,
-
-                    //calculete position of zoomed image.
-                    backgroundPositionX: `${-x * zoomLevel + magnifieWidth / 2}px`,
-                    backgroundPositionY: `${-y * zoomLevel + magnifierHeight / 2}px`
-                }}
-            ></div> */}
+          <span className="w-full break-all ">{formattedName}</span>
         </div>
-    );
-}
+      </div>
 
-export default Index
+      {/* <div
+        style={{
+          display: showMagnifier ? '' : 'none',
+          position: 'absolute',
+          pointerEvents: 'none',
+          height: `${magnifierHeight}px`,
+          width: `${magnifieWidth}px`,
+          top: `${position.y - magnifierHeight / 2}px`,
+          left: `${position.x - magnifieWidth / 2}px`,
+          opacity: '1',
+          border: '1px solid lightgray',
+          backgroundColor: 'white',
+          backgroundImage: `url('${src}')`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: `${imageSize.width * zoomLevel}px ${imageSize.height * zoomLevel}px`,
+          backgroundPositionX: `${-position.x * zoomLevel + magnifieWidth / 2}px`,
+          backgroundPositionY: `${-position.y * zoomLevel + magnifierHeight / 2}px`,
+        }}
+      ></div> */}
+    </div>
+  );
+};
+
+export default Index;
