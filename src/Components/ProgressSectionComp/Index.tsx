36a262d33d93353fad39783952883f6803eba269
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-29 04:37:03 -0700

    processTik

--- src/Components/ProgressSectionComp/Index.tsx
+++ src/Components/ProgressSectionComp/Index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
-
+import WhiteTik from '../../assets/svg/whiteCheck.svg'
+import { useSelector } from 'react-redux'
 const Index = () => {
+    let pageValue = useSelector((state: any) => state.InfoSlice?.page)
+    pageValue = typeof pageValue === 'object' ? (pageValue.payload) : pageValue
+    console.log('pageValue', pageValue)
     return (
         <div className='w-full'>
             <div className='w-full mt-8 flex justify-center'>
@@ -8,8 +12,12 @@ const Index = () => {
                     <div className='flex w-full translate-x-[25px]'>
                         {/* ---------------------------------------------------------------------------- */}
                         <div className='w-[10%] relative'>
-                            <div className='w-[35px] h-[35px] bg-[#4C6486] rounded-full flex justify-center items-center'>
-                                <span className='text-sm text-white'>1</span>
+                            <div className={`w-[35px] h-[35px] ${pageValue >= 1 ? 'bg-[#4C6486]' : null} rounded-full flex justify-center items-center`}>
+                                {pageValue <= 1 ?
+                                    <span className='text-sm text-white'>1</span> :
+                                    <span className='w-full flex justify-center items-center'>
+                                        <img className='w-[30%]' src={WhiteTik} />
+                                    </span>}
                             </div>
                             <div className='absolute top-0 left-0 w-full h-full flex items-end translate-y-8'>
                                 <span className='text-black text-sm'>İsim</span>
@@ -21,8 +29,11 @@ const Index = () => {
                         </div>
                         {/* ---------------------------------------------------------------------------- */}
                         <div className='w-[10%] relative'>
-                            <div className='w-[35px] h-[35px] bg-[#fff] rounded-full flex justify-center items-center border-[1px] border-[rgba(0,0,0,0.3)]'>
-                                <span className='text-sm text-black'>2</span>
+                            <div className={`w-[35px] h-[35px] ${pageValue >= 2 ? 'bg-[#4C6486]' : 'bg-[#fff]'} rounded-full flex justify-center items-center border-[1px] border-[rgba(0,0,0,0.3)]`}>
+                                {pageValue <= 2 ? <span className={`text-sm ${pageValue < 2 ? 'text-black' : 'text-white'}`}>2</span> : null}
+                                {pageValue > 2 ? <span className='w-full flex justify-center items-center'>
+                                    <img className='w-[30%]' src={WhiteTik} />
+                                </span> : null}
                             </div>
                             <div className='absolute top-0 left-0 w-full h-full flex items-end translate-y-8 -translate-x-5'>
                                 <span className='text-black text-sm'>Logo/Marka</span>
@@ -34,8 +45,11 @@ const Index = () => {
                         </div>
                         {/* ---------------------------------------------------------------------------- */}
                         <div className='w-[10%] relative'>
-                            <div className='w-[35px] h-[35px] bg-[#fff] rounded-full flex justify-center items-center border-[1px] border-[rgba(0,0,0,0.3)]'>
-                                <span className='text-sm text-black'>3</span>
+                            <div className={`w-[35px] h-[35px] ${pageValue >= 3 ? 'bg-[#4C6486]' : 'bg-[#fff]'} rounded-full flex justify-center items-center border-[1px] border-[rgba(0,0,0,0.3)]`}>
+                                {pageValue <= 3 ? <span className={`text-sm ${pageValue < 3 ? 'text-black' : 'text-white'}`}>3</span> : null}
+                                {pageValue > 3 ? <span className='w-full flex justify-center items-center'>
+                                    <img className='w-[30%]' src={WhiteTik} />
+                                </span> : null}
                             </div>
                             <div className='absolute top-0 left-0 w-full h-full flex items-end translate-y-8'>
                                 <span className='text-black text-sm'>Bayrak</span>
