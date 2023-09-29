8a2ca01dea4834bd32b41fd0f055ee9f886746e3
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-29 06:06:46 -0700

    proccessTik

--- src/Components/UploadSection/Upload.tsx
+++ src/Components/UploadSection/Upload.tsx
@@ -3,7 +3,7 @@ import add from '../../assets/svg/add.svg'
 const Upload = () => {
   return (
     <div className='w-full h-[140px] flex justify-center mb-8'>
-      <div className='w-[90%] h-full border-[1px] border-dashed border-[rgba(0,0,0,0.8)] rounded-md'>
+      <div className='w-[90%] h-full border-[2px] border-dashed border-[rgba(0,0,0,0.8)] rounded-md'>
         <div className='w-full flex justify-center pt-5'>
         <label className='w-[8%] cursor-pointer' htmlFor="upload">
             <img src={add} alt="Add" />
