ce19d6751e39d4b6982817cf235a3e5718bccc15
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-09-29 05:04:05 -0700

    updated

--- src/Components/UploadSection/Upload.tsx
+++ src/Components/UploadSection/Upload.tsx
@@ -5,13 +5,21 @@ const Upload = () => {
     <div className='w-full h-[140px] flex justify-center mb-8'>
       <div className='w-[90%] h-full border-[1px] border-dashed border-[rgba(0,0,0,0.8)] rounded-md'>
         <div className='w-full flex justify-center pt-5'>
-          <img className='w-[8%]' src={add} />
+        <label className='w-[8%] cursor-pointer' htmlFor="upload">
+            <img src={add} alt="Add" />
+          </label>
+          <input type='file' id='upload' hidden />
         </div>
         <div className='w-full flex justify-center pt-3'>
           <span className='text-sm font-bold'>Logo Yükle</span>
         </div>
         <div className='w-full flex justify-center pt-3'>
           <span className='text-xs text-[rgba(0,0,0,0.5)]'>Şeffaf arka plana sahip PNG görüntüleri olarak yükleyin</span>
+        </div>
+        <div className='w-full flex justify-center mt-4'>
+          <div className='h-[5px] rounded-md bg-[green] w-[90%]'>
+            
+          </div>
         </div>
       </div>
     </div>
